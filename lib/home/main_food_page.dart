56e2667fbacdcf34e25477d54d5691145e5c96b4
Hridoy-990 <_mjih_@Mac.communityfibre.co.uk>
2025-11-19 23:18:34 +0000

    small & big font added

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+import '../widgets/big_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -24,8 +27,13 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 children: [
                   Column(
                     children: [
-                      Text("Country"),
-                      Text("City")
+                      BigText(text: "Bangladesh", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: 'Sylhet', color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
 
                     ],
                   ),
