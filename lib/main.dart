6034d3a4927f7c2a46668f4712c2139aa919950a
Hridoy-990 <_mjih_@Mac.communityfibre.co.uk>
2025-12-05 23:57:14 +0000

    add new pages for popular food

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/home/main_food_page.dart';
+import 'package:food_delivery/page/food/popular_food_detail.dart';
+import 'package:food_delivery/page/home/main_food_page.dart';
 import 'package:get/get.dart';
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MainFoodPage(),
+      home: PopularFoodDetail(),
     );
   }
 }
