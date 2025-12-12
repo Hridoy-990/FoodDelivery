5c70430683363947d3ff48d6c42276cff241d17a
Hridoy-990 <_mjih_@Mac.communityfibre.co.uk>
2025-12-12 21:21:52 +0000

    api setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/page/food/popular_food_detail.dart';
 import 'package:food_delivery/page/food/recommended_food_detail.dart';
 import 'package:food_delivery/page/home/main_food_page.dart';
 import 'package:get/get.dart';
-void main() {
+import 'helper/dependencies.dart' as dep;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
@@ -13,6 +17,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.find<PopularProductController>().getPopularProductList();
+
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
