8b02418dd61d5c88f790af8d09fcf8e059321b1d
Hridoy-990 <_mjih_@Mac.communityfibre.co.uk>
2025-12-12 18:04:29 +0000

    recomanded page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/page/food/popular_food_detail.dart';
+import 'package:food_delivery/page/food/recommended_food_detail.dart';
 import 'package:food_delivery/page/home/main_food_page.dart';
 import 'package:get/get.dart';
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: PopularFoodDetail(),
+      home: RecommendedFoodDetail(),
     );
   }
 }
